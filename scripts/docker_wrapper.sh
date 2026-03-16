1b24a85cab268e6b1246fcbd767915d4583e694d
black-vajra <jonny@borderelliptic.com>
2026-03-16 09:04:01 -0400

    Add docker wrapper with dpkg-divert deployment notes

--- scripts/docker_wrapper.sh
+++ scripts/docker_wrapper.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Wrapper: injects --privileged --user 122:127 into docker run/create
 # Workaround for docker-ce 29.x + BOINC 8.2.8 LHC@home tmpfs incompatibility
-# Real binary: /usr/bin/docker.real
+# Real binary: /usr/bin/docker.real (managed by dpkg-divert)
+# Deployment: dpkg-divert --add --no-rename --divert /usr/bin/docker.real /usr/bin/docker
+# Removal: dpkg-divert --remove --no-rename /usr/bin/docker
 # See: docs/LHC_DOCKER_COMPATIBILITY.md
 
 REAL_DOCKER=/usr/bin/docker.real
